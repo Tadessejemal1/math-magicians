95e75a51d8dacae0d181acf0c4b595390d4f44df
Tadesse Jemal <tadessejemal63@gmail.com>
2022-09-07 23:12:32 +0300

    refactor with hooks

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,67 +1,58 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import Input from './Input';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
 
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-  }
+  const handleButtonClick = (name) => {
+    const { total: newTotal, next: newNext, operation: newOperation } = calculate(
+      { total, next, operation }, name,
+    );
 
-  handleButtonClick = (name) => {
-    const { total, next, operation } = calculate(this.state, name);
-    this.setState({
-      total,
-      next,
-      operation,
-    });
+    setTotal(newTotal);
+    setNext(newNext);
+    setOperation(newOperation);
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator">
-        <div className="row">
-          <Input text={next || operation || total} />
-        </div>
-        <div className="row">
-          <Button clickHandler={this.handleButtonClick} name="AC" />
-          <Button clickHandler={this.handleButtonClick} name="+/-" />
-          <Button clickHandler={this.handleButtonClick} name="%" />
-          <Button clickHandler={this.handleButtonClick} className="btn btn-operator" name="÷" />
-        </div>
-        <div className="row">
-          <Button clickHandler={this.handleButtonClick} name="7" />
-          <Button clickHandler={this.handleButtonClick} name="8" />
-          <Button clickHandler={this.handleButtonClick} name="9" />
-          <Button clickHandler={this.handleButtonClick} className="btn btn-operator" name="x" />
-        </div>
-        <div className="row">
-          <Button clickHandler={this.handleButtonClick} name="4" />
-          <Button clickHandler={this.handleButtonClick} name="5" />
-          <Button clickHandler={this.handleButtonClick} name="6" />
-          <Button clickHandler={this.handleButtonClick} className="btn-operator" name="-" />
-        </div>
-        <div className="row">
-          <Button clickHandler={this.handleButtonClick} name="1" />
-          <Button clickHandler={this.handleButtonClick} name="2" />
-          <Button clickHandler={this.handleButtonClick} name="3" />
-          <Button clickHandler={this.handleButtonClick} name="+" className="btn-operator" />
-        </div>
-        <div className="row">
-          <Button clickHandler={this.handleButtonClick} className="btn-zero" name="0" />
-          <Button clickHandler={this.handleButtonClick} name="." />
-          <Button clickHandler={this.handleButtonClick} name="=" className="btn-operator" />
-        </div>
+  return (
+    <div className="calculator">
+      <div className="row">
+        <Input text={next || operation || total} />
       </div>
-    );
-  }
-}
+      <div className="row">
+        <Button clickHandler={handleButtonClick} name="AC" />
+        <Button clickHandler={handleButtonClick} name="+/-" />
+        <Button clickHandler={handleButtonClick} name="%" />
+        <Button clickHandler={handleButtonClick} className="btn btn-operator" name="÷" />
+      </div>
+      <div className="row">
+        <Button clickHandler={handleButtonClick} name="7" />
+        <Button clickHandler={handleButtonClick} name="8" />
+        <Button clickHandler={handleButtonClick} name="9" />
+        <Button clickHandler={handleButtonClick} className="btn btn-operator" name="x" />
+      </div>
+      <div className="row">
+        <Button clickHandler={handleButtonClick} name="4" />
+        <Button clickHandler={handleButtonClick} name="5" />
+        <Button clickHandler={handleButtonClick} name="6" />
+        <Button clickHandler={handleButtonClick} className="btn-operator" name="-" />
+      </div>
+      <div className="row">
+        <Button clickHandler={handleButtonClick} name="1" />
+        <Button clickHandler={handleButtonClick} name="2" />
+        <Button clickHandler={handleButtonClick} name="3" />
+        <Button clickHandler={handleButtonClick} name="+" className="btn-operator" />
+      </div>
+      <div className="row">
+        <Button clickHandler={handleButtonClick} className="btn-zero" name="0" />
+        <Button clickHandler={handleButtonClick} name="." />
+        <Button clickHandler={handleButtonClick} name="=" className="btn-operator" />
+      </div>
+    </div>
+  );
+};
 export default Calculator;
